f4817f294fa919573c5f3585a430f66d851cde6a
Octavio Llanillo <llanillo4@hotmail.com>
2023-03-27 23:05:50 -0300

    Agrega ejercicios faltantes

--- TP1.sql
+++ TP1.sql
@@ -1,11 +1,13 @@
--- 1) Muestre el id, nombre, apellido y dni de los pacientes que tienen obra social. 113435
+-- 1) Muestre el id, nombre, apellido y dni de los pacientes que tienen obra social.
+-- 113435 filas.
 select p.id_paciente, per.nombre, per.apellido, per.dni
 from paciente p
          inner join obra_social ob using (id_obra_social)
          inner join persona per on p.id_paciente = per.id_persona;
 
 -- 2) Liste todos los pacientes con obra social que fueron atendidos en los consultorios 'CARDIOLOGIA' o
--- 'NEUMONOLOGIA'. Debe mostrar el nombre, apellido, dni y nombre de la obra social. 33716
+-- 'NEUMONOLOGIA'. Debe mostrar el nombre, apellido, dni y nombre de la obra social.
+-- 33716 filas
 select pa.id_paciente, per.nombre, per.apellido, per.dni
 from paciente pa
          inner join
@@ -26,7 +28,8 @@ where con.nombre in ('NEOMONOLOGIA', 'CARDIOLOGIA')
 
 --3) Liste el id, nombre, apellido y sueldo de los empleados, como así también su cargo y especialidad.
 --Ordenado alfabéticamente por cargo, luego por especialidad y en último término por sueldo de mayor a
---menor.- 996
+--menor.
+--996 filas
 
 select p.id_persona, p.nombre, p.apellido, em.sueldo, car.cargo, esp.especialidad
 from persona p
@@ -38,7 +41,8 @@ from persona p
 order by car.cargo, esp.especialidad, em.sueldo desc;
 
 -- 4) Encuentre el empleado, cargo y turno de todos los empleados cuyo cargo sea AUXILIAR y el turno de
--- trabajo aún se encuentre vigente. 170
+-- trabajo aún se encuentre vigente.
+-- 170 filas
 select p.id_persona, p.nombre, p.apellido, c.cargo, t.turno
 from persona p
          inner join empleado e on p.id_persona = e.id_empleado
@@ -50,7 +54,8 @@ where c.cargo like '%AUXILIAR%'
 
 -- 5) Muestre la cantidad de compras realizadas por los empleados de la especialidad SIN ESPECIALIDAD
 --MEDICA. Debe mostrar el nombre del empleado, el cargo que tiene y la cantidad de compras, ordenado
--- por cantidad de mayor a menor. 504
+-- por cantidad de mayor a menor.
+-- 504 filas
 select p.nombre, c.cargo, count(*) as cantidad_compras, es.especialidad
 from persona p
          inner join empleado e
@@ -63,7 +68,8 @@ group by p.id_persona, c.cargo, es.especialidad
 order by cantidad_compras desc;
 
 -- 6) Muestre los pacientes que tienen obra social, que fueron internados en septiembre del 2019, en el 7mo y
--- 8vo piso. Ordenados por la fecha de internación de mayor a menor. 2158
+-- 8vo piso. Ordenados por la fecha de internación de mayor a menor.
+-- 2158 filas
 select p.id_persona, p.nombre, int.fecha_inicio, ha.piso
 from persona p
          inner join paciente pa on p.id_persona = pa.id_paciente
@@ -75,21 +81,24 @@ where pa.id_obra_social is not null
   and int.fecha_inicio between '2019-09-01' and '2019-09-30'
 order by int.fecha_inicio desc;
 
--- 7) Muestre los proveedores a los que no se les compró ningún medicamento. 0
+-- 7) Muestre los proveedores a los que no se les compró ningún medicamento.
+-- 0 filas
 select p.proveedor
 from proveedor p
 where p.id_proveedor not in (select id_proveedor
                              from proveedor
                                       inner join compra c using (id_proveedor));
 
---8) Liste los medicamentos que no fueron prescriptos nunca. 3
+--8) Liste los medicamentos que no fueron prescriptos nunca.
+-- 3 filas
 select m.nombre
 from medicamento m
 where id_medicamento not in (select id_medicamento
                              from medicamento
                                       inner join tratamiento using (id_medicamento));
 
--- 9) Muestre los empleados que hayan realizado más internaciones que 'DAVID MASAVEU' antes del 15/02/2019. 22
+-- 9) Muestre los empleados que hayan realizado más internaciones que 'DAVID MASAVEU' antes del 15/02/2019.
+-- 22 filas
 select p.nombre, count(int.id_paciente) as inter_Resto
 from persona p
          inner join empleado e on p.id_persona = e.id_empleado
@@ -104,7 +113,8 @@ having count(int.id_paciente) > (select count(int.id_paciente) as inter_MASAVEU
                                    and p.apellido = 'MASAVEU'
                                    and int.fecha_inicio < '2019-02-15');
 
--- 10) Muestre los pacientes a los que les hayan facturado más que ‘LAURA MONICA JABALOYES’ desde el 15/05/2022 a la fecha. 554
+-- 10) Muestre los pacientes a los que les hayan facturado más que ‘LAURA MONICA JABALOYES’ desde el 15/05/2022 a la fecha.
+-- 554 filas
 select p.nombre, p.apellido, sum(fa.monto)
 from persona p
          inner join paciente pa on id_persona = id_paciente
@@ -123,24 +133,19 @@ order by sum(fa.monto);
 
 -- 11) Liste todos los empleados que no hayan comprado medicamentos del proveedor ‘ARABESA’ entre el
 -- 01/02/2018 y el 10/03/2018. Ordénelos alfabéticamente.
-select per.id_persona, per.nombre, per.apellido
-from persona per
-         inner join empleado empl on per.id_persona = empl.id_empleado
-         inner join compra com using (id_empleado)
-         inner join proveedor pro using (id_proveedor)
-where pro.proveedor like '%ARABESA'
-  and com.fecha between '2018-02-01' and '2018-03-10'
-  and per.id_persona
-    not in (select empl2.id_empleado
-            from persona
-                     inner join empleado empl2 on persona.id_persona = empl2.id_empleado
-                     inner join compra com2 on empl2.id_empleado = com2.id_empleado
-                     inner join proveedor pro2 on com2.id_proveedor = pro2.id_proveedor
-            where pro2.proveedor like '%ARABESA%'
-              and com2.fecha between '2018-02-01' and '2018-03-10')
-order by per.nombre;
+-- 971 filas
+select id_empleado, nombre
+from empleado
+         inner join persona on id_persona = id_empleado
+         inner join compra using (id_empleado)
+         inner join proveedor using (id_proveedor)
+where proveedor != 'ARABESA'
+  and fecha between '2018-02-01' and '2018-03-10'
+group by id_empleado, nombre
+order by nombre;
 
 -- 12) Muestre los 5 medicamentos más recetados y el laboratorio al que pertenecen.
+-- 5 filas.
 select med.id_medicamento,
        med.nombre,
        med.precio,
@@ -152,7 +157,6 @@ from medicamento med
 group by med.id_medicamento, med.nombre, med.precio, lab.laboratorio
 order by cantidad_veces_recetada desc
 limit 5;
-
 
 -- 13) Muestre (en una sola consulta) el id, fecha de ingreso y estado de todas las camas y equipos que aún no
 -- fueron reparadas.
@@ -186,8 +190,10 @@ where lab.laboratorio like '%LABOSINRATO%' and cla.clasificacion like '%APARATO 
 -- 15) Modifique el campo estado de la tabla mantenimiento_equipo, con la palabra “baja” y en la fecha de
 -- egreso ponga la fecha del sistema, de aquellos equipos que ingresaron hace más de 100 días (recalcule
 -- usando la fecha de ingreso y la del sistema)
-update mantenimiento_equipo set estado = 'baja', fecha_ingreso = current_date
-from;
+update mantenimiento_equipo
+set estado       = 'baja',
+    fecha_egreso = current_date
+where DATE_PART('day', AGE(NOW(), fecha_ingreso)) > 100;
 
 -- 16) Elimine las clasificaciones que no se usan en los medicamentos. 22 filas afectadas
 delete
