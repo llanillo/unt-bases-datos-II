fde020f70670d8a50614fa28b4b691fd20429e47
Octavio Llanillo <llanillo4@hotmail.com>
2023-05-03 18:10:16 -0300

    Agrega cositas

--- TP2.sql
+++ TP2.sql
@@ -3,85 +3,85 @@
 -- minuciosamente el o los campos por los cuales se realizarán las búsquedas).
 
 -- TABLA CAMA
-create index index_informacion_cama on cama(tipo, estado);
+create index index_informacion_cama on cama (tipo, estado);
 
 -- TABLA CARGO
-create index index_cargo on cargo(cargo);
+create index index_cargo on cargo (cargo);
 
 -- TABLA CLASIFICACIÓN
-create index index_clasificacion on clasificacion(clasificacion);
+create index index_clasificacion on clasificacion (clasificacion);
 
 -- TABLA COMPRA
-create index index_fecha_compra on compra(fecha);
+create index index_fecha_compra on compra (fecha);
 
 -- TABLA CONSULTA
-create index index_fecha_hora_consulta on consulta(fecha, hora);
+create index index_fecha_hora_consulta on consulta (fecha, hora);
 
 -- TABLA CONSULTORIO
-create index index_nombre_ubicacion_consultorio on consultorio(nombre, ubicacion);
+create index index_nombre_ubicacion_consultorio on consultorio (nombre, ubicacion);
 
 -- TABLA DIAGNÓSTICO
 
 -- TABLA EMPLEADO
-create index index_fecha_ingreso_empleado on empleado(fecha_ingreso);
-create index index_fecha_baja on empleado(fecha_baja);
+create index index_fecha_ingreso_empleado on empleado (fecha_ingreso);
+create index index_fecha_baja on empleado (fecha_baja);
 
 -- TABALA EQUIPO
-create index index_nombre_marca_equipo on equipo(nombre, marca);
+create index index_nombre_marca_equipo on equipo (nombre, marca);
 
 -- TABLA ESPECIALIDAD
 
 -- TABLA ESTUDIO
 
 -- TABLA ESTUDIO_REALIZADO
-create index index_resultado_estudio_realizado on estudio_realizado(resultado);
+create index index_resultado_estudio_realizado on estudio_realizado (resultado);
 
 -- TABLA FACTURA
-create index index_fecha_factura on factura(fecha);
+create index index_fecha_factura on factura (fecha);
 
 -- TABLA HABITACIÓN
-create index index_informacion_habitacion on habitacion(piso, numero);
+create index index_informacion_habitacion on habitacion (piso, numero);
 
 -- TABLA INTERNACIÓN
-create index index_fecha_alta_internacion on internacion(fecha_alta);
+create index index_fecha_alta_internacion on internacion (fecha_alta);
 
 -- TABLA LABORATORIO
-create index index_nombre_laboratorio on laboratorio(laboratorio);
+create index index_nombre_laboratorio on laboratorio (laboratorio);
 
 -- TABLA MANTENIMIENTO_CAMA
-create index index_fecha_ingreso_mantenimiento_cama on mantenimiento_cama(fecha_ingreso);
+create index index_fecha_ingreso_mantenimiento_cama on mantenimiento_cama (fecha_ingreso);
 
 -- YABLA MANTEMINIMIENTO_EQUIPO
-create index index_fecha_ingreso_mantenimiento_equipo on mantenimiento_equipo(fecha_ingreso);
+create index index_fecha_ingreso_mantenimiento_equipo on mantenimiento_equipo (fecha_ingreso);
 
 -- TABLA MEDICAMENTO
-create index index_medicamente on medicamento(nombre, presentacion);
+create index index_medicamente on medicamento (nombre, presentacion);
 
 -- TABLA OBRA_SOCIAL
-create index index_informacion_obra_social on obra_social(nombre, provincia, localidad);
+create index index_informacion_obra_social on obra_social (nombre, provincia, localidad);
 
 -- TABLA PACIENTE
 
 -- TABLA PAGO
-create index index_fecha_pago on pago(fecha);
+create index index_fecha_pago on pago (fecha);
 
 -- TABLA PATOLOGÍA
-create index index_nombre_patologia on patologia(nombre);
+create index index_nombre_patologia on patologia (nombre);
 
 -- TABLA PERSONA
-create index index_nombre_completo_persona on persona(apellido, nombre);
-create index index_dni_persona on persona(dni);
+create index index_nombre_completo_persona on persona (apellido, nombre);
+create index index_dni_persona on persona (dni);
 
 -- TABLA PROVEEDOR
-create index index_informacion_proveedor on proveedor(proveedor, direccion);
+create index index_informacion_proveedor on proveedor (proveedor, direccion);
 
 -- TABLA TIPO_ESTUIDO
 
 -- TABLA TRABAJAN
-create index index_inicio_trabajan on trabajan(inicio);
+create index index_inicio_trabajan on trabajan (inicio);
 
 -- TABLA TRATAMIENTO
-create index index_informacion_tratamiento on tratamiento(nombre, fecha_indicacion);
+create index index_informacion_tratamiento on tratamiento (nombre, fecha_indicacion);
 
 -- TABLA TURNO
 
