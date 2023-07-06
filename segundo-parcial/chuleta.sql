850a7b5f07467c06fe8a4ed1bbeb53171d26f3c8
Octavio Llanillo <llanillo4@hotmail.com>
2023-07-06 10:31:43 -0300

    Agrega condiciones a triggers

--- segundo-parcial/chuleta.sql
+++ segundo-parcial/chuleta.sql
@@ -3,3 +3,8 @@ Consultas dinámicas
 %s se usa para cadenas de caracteres
 %I se usa para identificador de SQL (tablas, columnas)
 %L se usa argumento literal SQL(“tablas", "columnas”)
+
+
+Triggers
+Si es After se pone solo return
+Si es before hay que pensar si retorno el nuevo o el viejo
