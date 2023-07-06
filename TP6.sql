21651118713e8aa7325ba1ba9348ff9337464dcb
Octavio Llanillo <llanillo4@hotmail.com>
2023-07-05 22:36:11 -0300

    Corrige errores y agrega llamadas a los SP

--- TP6.sql
+++ TP6.sql
@@ -1,5 +1,5 @@
 --  Ejercicio nro. 1:
--- Para mejorar y automatizar el funcionamiento de la base de datos “Hospital” realice las siguientes
+--
 -- tareas:
 -- a) Escriba un procedimiento almacenado (SP) para agregar registros a la tabla persona. Reciba
 -- todos los parámetros necesarios excepto el id (max + 1) que se deberá obtener dentro del SP.
@@ -10,7 +10,25 @@ create or replace procedure sp_persona_alta(nombre varchar(100), apellido varcha
                                             domicilio varchar(100),
                                             telefono varchar(15)) AS
 $$
-BEGIN
+declare
+    existe_dni bool;
+begin
+    if nombre is null or nombre = '' then
+        raise exception 'El nombre es obligatorio';
+    end if;
+
+    if apellido is null or apellido = '' then
+        raise exception 'El apellido es obligatorio';
+    end if;
+
+    if dni is null or dni = '' then
+        raise exception 'El DNI es obligatorio';
+    end if;
+
+    if exists(select 1 from persona p where p.dni like $3) then
+        raise exception 'Ya existe una persona con el DNI ingresado';
+    end if;
+
     insert into persona values ((select max(id_persona) + 1 from persona), $1, $2, $3, $4, $5, $6);
     raise notice 'Se insertó la persona %, % de manera exitosa', $1, $2;
 exception
@@ -24,20 +42,25 @@ $$ language plpgsql;
 -- especialidad y el nombre del cargo. Verifique que dichos datos existan para poder hacer el alt
 -- Nombre sugerido: empleado_alta.
 
-create or replace procedure sp_empleado_alta(dni varchar(8), especialidad varchar(100), cargo varchar(100),
-                                             sueldo numeric(9, 2)) as
+create or replace procedure sp_empleado_alta(p_dni varchar(8), p_especialidad varchar(100), p_cargo varchar(100),
+                                             p_fecha_ingreso date, p_sueldo numeric(9, 2), p_fecha_baja date) as
 $$
-declare
-    existe_persona      bool;
-    existe_cargo        bool;
-    existe_especialidad bool;
-BEGIN
-    select exists(select 1 from persona p where p.dni like $1) into existe_persona;
-    select exists(select 1 from especialidad e where e.especialidad like $2) into existe_especialidad;
-    select exists(select 1 from cargo c where c.cargo like $3) into existe_cargo;
+begin
 
-    if not existe_cargo or not existe_especialidad or not existe_persona then
-        raise exception 'No existe el cargo o la especialidad o la persona';
+    if not exists(select * from persona p where p.dni = p_dni) then
+        raise exception 'No existe una persona con el dni ingresado';
+    end if;
+
+    if not exists(select * from especialidad where especialidad = p_especialidad) then
+        raise exception 'No existe la especialidad ingresada';
+    end if;
+
+    if not exists(select * from cargo where cargo = p_cargo) then
+        raise exception 'No existe el cargo ingresado';
+    end if;
+
+    if p_fecha_baja < p_fecha_ingreso then
+        raise exception 'La fecha de baja no puede ser anterior a la fecha de ingreso';
     end if;
 
     insert into empleado
@@ -61,11 +84,22 @@ create or replace procedure sp_factura_modifica_saldo(id_factura bigint, monto n
 $$
 declare
     existe_factura boolean;
+    saldo_factura  numeric(10, 2);
 begin
     select exists(select 1 from factura f where f.id_factura = $1) into existe_factura;
 
     if not existe_factura then
-        raise exception 'No exiset la factura';
+        raise exception 'No el número de factura ingresado';
+    end if;
+
+    if monto is null or monto < 0 then
+        raise exception 'Se debe ingresar un monto positivo como pago';
+    end if;
+
+    select saldo from factura f where f.id_factura = $1 into saldo_factura;
+
+    if monto > saldo_factura then
+        raise exception 'No se puede realizar un pago mayor a lo adeudado';
     end if;
 
     update factura set saldo = saldo - $2 where id_factura = $1;
@@ -89,15 +123,22 @@ begin
     select exists(select 1 from laboratorio l where l.laboratorio like $1) into existe_laboratorio;
 
     if not existe_laboratorio then
-        raise exception 'Error en la modificación del precio del medicamento %', SQLERRM;
+        raise exception 'No existe el laboratorio indicado';
+    end if;
+
+    if porcentaje is null or porcentaje < 0 then
+        raise exception 'El porcentaje ingresado es inválido';
     end if;
 
     update medicamento
-    set precio = precio * (porcentaje / 100 + 1)
+    set precio = precio * (1 + porcentaje / 100)
     where id_laboratorio = (select id_laboratorio from laboratorio l where laboratorio like $1);
+
     raise notice 'Se modificó exitosamente el precio del medicamento';
 
-end;
+exception
+    when others then raise exception 'Error en la modificación del precio de los medicamentos %', SQLERRM;
+end ;
 $$ language plpgsql;
 
 -- e) Realice un SP para eliminar un medicamento según su nombre. Recuerde que puede estar
@@ -108,23 +149,25 @@ create or replace procedure sp_medicamento_eliminar(nombre_medicamento varchar(5
 as
 $$
 declare
-    id_medicamento_a_borrar int;
-    existe_medicamento      bool;
+    id_medicamento_borrar int;
+    existe_medicamento    bool;
 begin
     select exists(select id_medicamento from medicamento m where m.nombre like nombre_medicamento);
 
     if not existe_medicamento then
-        raise exception 'Error en la eliminación del medicamento %', SQLERRM;
+        raise exception 'No existe el medicamento ingresado';
     end if;
 
-    select id_medicamento from medicamento m where m.nombre like nombre_medicamento into id_medicamento_a_borrar;
+    select id_medicamento from medicamento m where m.nombre like nombre_medicamento into id_medicamento_borrar;
 
-    update tratamiento set id_medicamento = null where id_medicamento = id_medicamento_a_borrar;
-    delete from compra where id_medicamento = id_medicamento_a_borrar;
-    delete from medicamento where id_medicamento = id_medicamento_a_borrar;
+    update tratamiento set id_medicamento = null where id_medicamento = id_medicamento_borrar;
+    delete from compra where id_medicamento = id_medicamento_borrar;
+    delete from medicamento where id_medicamento = id_medicamento_borrar;
 
     raise notice 'Se elimino exitosamente el medicamento de la base de datos (Incluyendo todas sus referencias)';
 
+exception
+    when others then raise exception 'Error en el eliminado del medicamento %', SQLERRM;
 end;
 $$ language plpgsql;
 
@@ -133,89 +176,94 @@ $$ language plpgsql;
 -- a) Un SP que muestre el nombre y apellido de un paciente según un DNI ingresado.Nombre
 -- sugerido: paciente_obtener.
 
-create or replace procedure sp_paciente_obtener(dni_buscado varchar(8))
+create or replace procedure sp_paciente_obtener(dni_buscado varchar, out p_nombre varchar, out p_apellido varchar)
 as
 $$
-declare
-    paciente_obtenido persona;
 begin
-    select * from persona p where dni like $1 into paciente_obtenido;
-    raise notice 'El paciente es %, %', paciente_obtenido.nombre, paciente_obtenido.apellido;
+    select p.nombre, p.apellido from persona p where dni like $1 into p_nombre, p_apellido;
+
 exception
     when others then raise exception 'Errors en la búsqueda del paciente %', SQLERRM;
 end;
 $$ language plpgsql;
 
+call sp_paciente_obtener('37870755', null, null);
+
 -- b) Un SP que muestre el precio y stock de un medicamente.Debe recibir como parámetro el
 -- nombre del medicamento.Nombre sugerido: medicamento_precio_stock.
 
-create or replace procedure sp_medicamento_precio_stock(nombre_medicamento varchar(50))
+create or replace procedure sp_medicamento_precio_stock(nombre_medicamento varchar, out p_precio numeric(8, 2),
+                                                        out p_stock int)
 as
 $$
-declare
-    medicamento_buscado medicamento;
 begin
-    select * from medicamento m where nombre like $1 into medicamento_buscado;
-    raise notice 'El medicamento tiene precio % y stock %', medicamento_buscado.precio, medicamento_buscado.stock;
+    select m.precio, m.stock from medicamento m where nombre like $1 into p_precio, p_stock;
 exception
     when others then raise exception 'Error en la búsqueda del medicamento %', SQLERRM;
 end;
-
 $$ language plpgsql;
+
+call sp_medicamento_precio_stock('PANADOL MASTICABLE NINOS', null, null);
 
 -- c) Escriba un SP que muestre el total adeudado (campo saldo de las facturas) por un paciente,
 -- según un número de DNI ingresado.Nombre sugerido: paciente_deuda.
 
-create or replace procedure sp_paciente_deuda(dni varchar(8))
+create or replace procedure sp_paciente_deuda(dni varchar, out p_saldo numeric)
 as
 $$
 declare
     existe_paciente bool;
-    total_adeudado  float;
 begin
-    select exists(select 1 from persona p where p.dni like $1);
+    select exists(select 1 from persona p where p.dni like $1) into existe_paciente;
 
-    if existe_paciente then
-        select sum(saldo)
-        from persona p
-                 inner join paciente pa on p.id_persona = pa.id_paciente
-                 inner join factura f using (id_paciente)
-        into total_adeudado;
-        raise notice 'Total adeudado es %', total_adeudado;
+    if not existe_paciente then
+        raise exception 'No existe el usuario ingresado';
     end if;
+
+    select sum(saldo)
+    from persona p
+             inner join paciente pa on p.id_persona = pa.id_paciente
+             inner join factura f using (id_paciente)
+    where p.dni like $1
+    into p_saldo;
+
 exception
     when others then raise exception 'Error en la búsqueda del total adeudado por el paciente %', SQLERRM;
 end;
 $$
     language plpgsql;
 
+call sp_paciente_deuda('6284417', null);
+
 -- d) Realice un SP que muestre la cantidad de veces que una cama estuvo en mantenimiento, se
 -- debe mandar como parámetro el id de la cama. Nombre sugerido:
 -- cama_cantidad_mantenimiento.
-create or replace procedure sp_cama_cantidad_mantenimiento(id_cama bigint)
+
+create or replace procedure sp_cama_cantidad_mantenimiento(id_cama bigint, out total_veces int)
 as
 $$
 declare
     existe_cama bool;
-    total_veces int;
 begin
-    select exists(select 1 from cama c where c.id_cama = $1);
+    select exists(select 1 from cama c where c.id_cama = $1) into existe_cama;
 
-    if existe_cama then
-
-        select count(*)
-        from cama c
-                 inner join mantenimiento_cama mc using (id_cama)
-        where c.id_cama = $1
-        into total_veces;
-        raise notice 'La cantidad de veces que una cama estuvo en mantenimiento %', total_veces;
+    if not existe_cama then
+        raise exception 'No existe la cama ingresada';
     end if;
+
+    select count(*)
+    from cama c
+             inner join mantenimiento_cama mc using (id_cama)
+    where c.id_cama = $1
+    into total_veces;
+
 exception
     when others then raise exception 'Error en la búsqueda de la cama %', SQLERRM;
 end;
 $$
     language plpgsql;
 
+call sp_cama_cantidad_mantenimiento(1, null);
 
 -- Ejercicio nro. 3:
 -- Realice los siguientes procedimientos almacenados utilizando cursores.
@@ -245,8 +293,11 @@ end;
 $$
     language plpgsql;
 
+call sp_obra_social_listado();
+
 -- b) Realice un SP donde se listen todas las camas
 -- cuyo estado sea “OK”. Nombre sugerido: cama_listado_ok
+
 create or replace procedure sp_cama_listado_ok() as
 
 $$
@@ -266,6 +317,8 @@ begin
 end;
 $$
     language plpgsql;
+
+call sp_cama_listado_ok();
 
 -- c) Realice un SP que liste todos los medicamentos cuyo stock sea
 -- menor que 50. Nombre sugerido: medicamentos_poco_stock.
@@ -290,6 +343,8 @@ begin
 end;
 $$
     language plpgsql;
+
+call sp_medicamentos_poco_stock();
 
 -- d) Escriba un SP que muestre todas las consultas realizadas en determinada fecha (no haga
 -- JOINS). Debe recibir por parámetro la fecha. Nombre sugerido: consulta_listado_por_fecha.
@@ -321,37 +376,32 @@ $$ language plpgsql;
 -- estudios que se realizó. Debe recibir como parámetro el DNI del paciente. Nombre sugerido:
 -- estudio_por_paciente.
 
-drop type if exists paciente_estudio;
-create type paciente_estudio as
-(
-    paciente persona,
-    estudio  estudio_realizado
-);
-
-create or replace procedure sp_estudio_por_paciente(dni varchar(8))
+create or replace procedure sp_estudio_por_paciente(dni varchar)
 as
 $$
 declare
-    cursor_paciente_estudio cursor for select p.*, e.*
+    cursor_paciente_estudio cursor for select p.nombre, p.apellido, e.nombre, er.fecha
                                        from persona p
                                                 inner join paciente pa on p.id_persona = pa.id_paciente
-                                                inner join estudio_realizado e using (id_paciente)
+                                                inner join estudio_realizado er using (id_paciente)
+                                                inner join estudio e using (id_estudio)
                                        where p.dni like $1;
-    paciente_estudio_fila paciente_estudio;
-
+    paciente_estudio_fila record;
 begin
     open cursor_paciente_estudio;
     loop
         fetch cursor_paciente_estudio into paciente_estudio_fila;
         exit when not found;
         raise notice 'Nombre=%, Apellido=%, Fecha=%, Estudio=%',
-            paciente_estudio_fila.paciente.nombre, paciente_estudio_fila.paciente.apellido,
-            paciente_estudio_fila.estudio.fecha, paciente_estudio_fila.estudio.resultado;
+            paciente_estudio_fila.nombre, paciente_estudio_fila.apellido,
+            paciente_estudio_fila.fecha, paciente_estudio_fila.nombre;
     end loop;
 
     close cursor_paciente_estudio;
 end;
 $$ language plpgsql;
+
+call sp_estudio_por_paciente('6171789');
 
 -- call sp_estudio_por_paciente('17728326');
 
@@ -359,7 +409,7 @@ $$ language plpgsql;
 -- un determinado turno. Debe recibir por parámetro el nombre del turno. Nombre sugerido:
 -- empleado_por_turno.
 
-create or replace procedure sp_empleado_por_turno(turno varchar(25))
+create or replace procedure sp_empleado_por_turno(turno varchar)
 as
 $$
 declare
@@ -382,18 +432,18 @@ begin
 end;
 $$ language plpgsql;
 
--- call sp_empleado_por_turno('full');
+call sp_empleado_por_turno('full');
 
 -- Ejercicio nro. 4:
 -- También usando cursores, realice los siguientes procedimientos almacenados con consultas más
 -- complejas.
+
 -- a) Un SP con los datos de los medicamentos, de un determinado laboratorio y clasificación,
 -- cuyo precio sea menor que el promedio de precios de todos los medicamentos de ese
 -- laboratorio y clasificación. Debe recibir por parámetro el nombre del laboratorio y el
 -- nombre de la clasificación. Nombre sugerido: medicamento_laboratorio_clasificacion.
 
-create or replace procedure sp_medicamento_laboratorio_clasificacion(lab varchar(50), clas varchar(75))
-as
+create or replace procedure sp_medicamento_laboratorio_clasificacion(p_laboratorio varchar, p_clasificacion varchar) as
 $$
 declare
     cursor_medicamento cursor for select *
@@ -412,19 +462,17 @@ declare
 begin
     open cursor_medicamento;
     loop
-        fetch cursor_medicamento into medicamento;
-        exit when not found;
+        fetch cursor_medicamento into medicamento; exit when not found;
         raise notice 'Nombre=%, Presentación=%, Precio=%, Stock=%', medicamento.nombre, medicamento.presentacion, medicamento.precio, medicamento.stock;
     end loop;
-
     close cursor_medicamento;
 end;
-
 $$
     language plpgsql;
 
 -- b) Un SP que muestre los datos de los 10 pacientes a los cuales más se le facturó (tome en
 -- cuenta el monto de las facturas). Nombre sugerido: factura_top_ten.
+
 create or replace procedure sp_factura_top_ten()
 as
 $$
